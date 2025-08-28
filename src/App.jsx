ab4f3f784f4ffa501d8b44e59f7fbdc016327ce3
Ângela Cavunduri <angelacavundurijulcira@gmail.com>
2025-08-28 17:53:19 +0100

    State lift

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,17 @@ import './App.css'
 import OutraLista from './Components/OutraLista'
 import HelloWorld from './Components/HelloWorld.jsx'
 import SayMyName from './Components/SayMyName.jsx'
+import SeuNome from './Components/SeuNome.jsx'
+import Saudacao from './Components/Saudacao.jsx'
 
 function App(){
-  const MeusItens=["React","Vue","Angular"]
+  const [nome,SetNome]=useState()
 
   return(
     <>
-    <h1>Renderização de listas</h1>
-    <OutraLista itens={MeusItens}/>
-     <OutraLista itens={[]}/>
-      <div>
-       
-       
-      </div>
-      <p className="read-the-docs">
-         <HelloWorld/>
-         <SayMyName nome="Ângela"/>
-      </p>
+    <h1>State Lift</h1>
+    <SeuNome setNome={SetNome}/>
+    <Saudacao nome={nome}/>
     </>
   )
 }
